91458dcc8c85fd4b6882b2c1d098fe83290165cf
MaximRybak <mrybak@luxoft.com>
2018-12-26 12:13:03 +0200

    Add cmakefile and main function

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <experimental/filesystem>
+#include <experimental/string_view>
+#include <thread>
+using namespace std;
+namespace fs = std::experimental::filesystem;
+
+int main()
+{
+    std::thread th([](){ cout << "Test" << endl; });
+    th.join();
+    cout << "Hello World!" << endl;
+    return 0;
+}
